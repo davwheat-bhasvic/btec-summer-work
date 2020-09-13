ee25db969f95e1e49a4d35d76cdc5e6addc7d811
David Wheatley <hi@davwheat.dev>
2020-09-13 17:25:01 +0100

    Add program 7

---
+++ 7-basic-calculator.py
@@ -0,0 +1,9 @@
+# This only works with integers
+# This does NOT work with floats and does not handle string inputs
+
+num1 = input("num1: ")
+num2 = input("num2: ")
+
+result = int(num1) + int(num2)
+
+print(result)
