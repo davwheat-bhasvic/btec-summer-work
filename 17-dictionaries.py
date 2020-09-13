4f96fb5fdebe82ec3f4037d7a60bcbdbc6e62353
David Wheatley <hi@davwheat.dev>
2020-09-13 19:58:49 +0100

    Add program 17

---
+++ 17-dictionaries.py
@@ -0,0 +1,18 @@
+months = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December",
+}
+
+print(months.get("Dec"))
+print(months.get("XXX"))
+print(months.get("XXX", "Unknown"))
