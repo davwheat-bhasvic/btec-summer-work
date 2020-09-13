2ac3611dd6e464542837d84a499475321ccf01bb
David Wheatley <hi@davwheat.dev>
2020-09-13 17:31:23 +0100

    Add program 9

---
+++ 9-lists.py
@@ -0,0 +1,17 @@
+my_friends = [
+    "Dave",
+    "John",
+    "Barry",
+    "Magaret",
+    "Lottie",
+    "Harry",
+    "Matty",
+    "Josh",
+    "Andrew",
+]
+
+print(my_friends[0])
+my_friends[0] = "Jimbob"
+print(my_friends[0])
+
+print(my_friends)
