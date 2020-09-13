86f530427490e579018245ee52b5b1f4340446d4
David Wheatley <hi@davwheat.dev>
2020-09-13 17:03:05 +0100

    Add program 5

---
+++ 5-working-with-numbers.py
@@ -0,0 +1,17 @@
+import math
+
+int_a = 2
+int_b = int_a * 5
+
+float_a = 2.5
+float_b = float_a * 3
+
+print(int_a, "+", int_b, "=", int_a + int_b)
+print(int_a, "x", int_b, "=", int_a * int_b)
+
+print(int_a, "+", float_a, "=", int_a + float_a)
+print(float_a, "+", float_b, "=", float_a + float_b)
+
+print(int_b, "mod", int_a + 1, "=", int_b % int_b + 1)
+
+print("sqrt(25)", "=", math.sqrt(25))
