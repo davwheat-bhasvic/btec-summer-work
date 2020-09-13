fbf7fed6cb852fe72cbc55bb571aafbf7d34e13c
David Wheatley <hi@davwheat.dev>
2020-09-13 23:27:24 +0100

    Add program 32

---
+++ 32-inheritance/Chef.py
@@ -0,0 +1,6 @@
+class Chef:
+    def make_chicken(self):
+        print("The chef makes chicken")
+
+    def make_special(self):
+        print("The chef makes a cottage pie")
---
+++ 32-inheritance/app.py
@@ -0,0 +1,8 @@
+from Chef import Chef
+from ChineseChef import ChineseChef
+
+c = Chef()
+cc = ChineseChef()
+
+c.make_special()
+cc.make_special()
