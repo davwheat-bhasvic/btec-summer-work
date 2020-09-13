86d9559b683d5658412c01fc1bee6ac9ea789713
David Wheatley <hi@davwheat.dev>
2020-09-13 19:15:08 +0100

    Add program 14

---
+++ 14-if-statements.py
@@ -0,0 +1,11 @@
+is_male = True
+is_tall = True
+
+if is_male and is_tall:
+    print("You are a tall male")
+elif is_male:
+    print("You are a male")
+elif is_tall:
+    print("You are tall")
+else:
+    print("You are neither male nor tall")
