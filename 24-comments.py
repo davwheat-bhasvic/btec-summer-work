281994d33684c2f934fa97b957e1976ba3514970
David Wheatley <hi@davwheat.dev>
2020-09-13 20:30:48 +0100

    Add program 24

---
+++ 24-comments.py
@@ -0,0 +1,18 @@
+"""
+I'm a multiline comment (string)
+"""
+
+"Technically this works as a comment to"
+# but this is better
+
+# Personally, I prefer multi-line
+# comments like this over the string
+# comments
+
+""" 
+But that's up to you to decide
+
+print('this doesn't work')
+"""
+
+print("this does work")
