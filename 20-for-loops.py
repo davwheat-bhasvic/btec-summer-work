406464825824c4521fef0aac66f25a4995937b5d
David Wheatley <hi@davwheat.dev>
2020-09-13 20:02:25 +0100

    Add program 20

---
+++ 20-for-loops.py
@@ -0,0 +1,20 @@
+friends = [
+    "Harry",
+    "Josh",
+    "Matty",
+    "Andrew",
+]
+
+for index in range(len(friends)):
+    if index == 0:
+        print("first iteration")
+    else:
+        print("not first")
+
+    print(friends[index])
+
+print("\n\n")
+
+# For each loops!
+for friend in friends:
+    print(friend)
