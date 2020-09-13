f03eb979c1e9d9766ab844f358032b9950077a1b
David Wheatley <hi@davwheat.dev>
2020-09-13 16:16:49 +0100

    Add program 3

---
+++ 3-variables-and-data-types.py
@@ -0,0 +1,10 @@
+character_name = "David"
+character_age = "16"
+
+print("There once was a man named " + character_name + ",")
+print("he was " + character_age + " years old.")
+
+character_name = "Dave"
+
+print("He really liked the name " + character_name + ",")
+print("but didn't like being " + character_age + ".")
