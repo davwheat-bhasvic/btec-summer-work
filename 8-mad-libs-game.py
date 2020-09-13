8023e26f3570e24905fed95c32a6f0c3949cf970
David Wheatley <hi@davwheat.dev>
2020-09-13 17:28:11 +0100

    Add program 8

---
+++ 8-mad-libs-game.py
@@ -0,0 +1,7 @@
+colour = input("Enter a colour: ")
+pl_noun = input("Enter a pl. noun: ")
+celebrity = input("Enter a celebrity name: ")
+
+print(f"\nRoses are {colour}")
+print(f"{pl_noun} are blue")
+print(f"I love {celebrity}")
