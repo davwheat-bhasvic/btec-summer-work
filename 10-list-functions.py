19e3dfdc5ccb8082536438aa6a61f72dfb249055
David Wheatley <hi@davwheat.dev>
2020-09-13 17:51:31 +0100

    Add program 10

---
+++ 10-list-functions.py
@@ -0,0 +1,9 @@
+lucky_numbers = [1, 8, 69, 420, 12345]
+
+print("My lucky numbers:", lucky_numbers)
+
+lucky_numbers.reverse()
+print("My lucky numbers reversed:", lucky_numbers)
+
+copied_numbers = lucky_numbers.copy()
+print("My copied numbers:", copied_numbers)
